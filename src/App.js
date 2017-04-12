41c8d12f458ce709fbe0b706b03ef78bd654774f
cristianhoyos66 <cristianhoyos66@hotmail.com>
2017-04-11 23:09:36 -0500

    Adding details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, {Component} from 'react';
-import './App.css';
 import firebase from 'firebase';
+import logo from './upload.png';
+
+import './App.css';
+import FileUpload from './FileUpload';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      pictures: []
+    };
+
+    this.handleAuth = this
+      .handleAuth
+      .bind(this);
+    this.handleUpload = this
+      .handleUpload
+      .bind(this);
+  }
+
+  componentWillMount() {
+    firebase
+      .auth()
+      .onAuthStateChanged(user => {
+        this.setState({user});
+      });
+
+    firebase
+      .database()
+      .ref('pictures')
+      .on('child_added', snapshot => {
+        this.setState({
+          pictures: this
+            .state
+            .pictures
+            .concat(snapshot.val())
+        });
+      });
+  }
 
   handleAuth() {
     const provider = new firebase
@@ -16,15 +53,90 @@ class App extends Component {
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
+  handleLogout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleUpload(event) {
+    const file = event.target.files[0];
+    const storageRef = firebase
+      .storage()
+      .ref(`fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      this.setState({uploadValue: percentage})
+    }, error => {
+      console.error(error.message);
+    }, () => {
+      const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      }
+      const dbRef = firebase
+        .database()
+        .ref('pictures');
+      const newPicture = dbRef.push();
+      newPicture.set(record);
+    });
+  }
+
+  renderLoginButton() {
+    if (!this.state.user) {
+      return (
+        <button onClick={this.handleAuth} className="App-btn">
+          Iniciar sesión con Google
+        </button>
+      );
+    } else {
+      return (
+        <div className="App-intro">
+          <p className="App-intro">¡Hola, {this.state.user.displayName}!</p>
+
+          <button onClick={this.handleLogout} className="App-btn">
+            Salir
+          </button>
+
+          <FileUpload uploadValue={this.state.uploadValue} onUpload={this.handleUpload}/> {this
+            .state
+            .pictures
+            .map(picture => (
+              <div className="App-card">
+                <figure className="App-card-image">
+                  <img width="320" src={picture.image} alt="..."/>
+                  <figCaption className="App-card-footer">
+                    <img
+                      className="App-card-avatar"
+                      src={picture.photoURL}
+                      alt={picture.displayName}/>
+                    <span className="App-card-name">{picture.displayName}</span>
+                  </figCaption>
+                </figure>
+              </div>
+            ))
+            .reverse()
+}
+
+        </div>
+
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Pseudogram</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Upload your picture now</h2>
         </div>
-        <p className="App-Intro">
-          <button onClick={this.handleAuth}>Login con Google</button>
-        </p>
+        {this.renderLoginButton()}
       </div>
     );
   }
