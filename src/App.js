100caf6ed21a731ff78068da23fbd00c90280b69
cristianhoyos66 <cristianhoyos66@hotmail.com>
2017-04-11 21:06:37 -0500

    Adding google authentication

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
+
+  handleAuth() {
+    const provider = new firebase
+      .auth
+      .GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Pseudogram</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-Intro">
+          <button onClick={this.handleAuth}>Login con Google</button>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDwDzATOy0V8OMIWf1W2abnWJW4VN6sohc",
+  authDomain: "pseudogram-4d9b7.firebaseapp.com",
+  databaseURL: "https://pseudogram-4d9b7.firebaseio.com",
+  projectId: "pseudogram-4d9b7",
+  storageBucket: "pseudogram-4d9b7.appspot.com",
+  messagingSenderId: "20564914047"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <App/>, document.getElementById('root'));
